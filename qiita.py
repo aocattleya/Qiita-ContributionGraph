1b6a505c8f3bae08dca21b10e78a7c781996edc1
aocattleya <aocattleya@yahoo.co.jp>
2020-02-13 04:41:17 +0900

    initial commit

---
+++ qiita.py
@@ -0,0 +1,41 @@
+'''--------------------------------
+Qiitaいいね数の上位5位までを書いてね！
+あとは、python qiita.pyを実行だ(*･ᴗ･*)و
+--------------------------------'''
+contributions = [1005, 322, 299, 90, 46]
+
+
+'''--------------------------------
+集計結果を出力する為に色々してる所だよ！
+リファクタリング待ってて( ´•௰• ` )
+--------------------------------'''
+total = sum(contributions)
+
+for contribution in contributions:
+  print(contribution, end="")
+  num = 0
+  space = 4 - len(str(contribution))
+  while num < space:
+      print(' ', end="")
+      num += 1
+
+  print('  Contribution  ', end="")
+
+  percentage = round(contribution / total * 10) * 2
+  num = 0
+  while num < percentage:
+      print('█', end="")
+      num += 1
+  print('▎', end="")
+  other = 20 - percentage
+  num = 0
+  while num < other:
+      print('░', end="")
+      num += 1
+
+  print('  ', end="")
+  other_pace = str(round(contribution / total * 100))
+  other_pace_len = int(len(other_pace))
+  if other_pace_len == 1:
+    print(' ', end="")
+  print(other_pace + '%')
